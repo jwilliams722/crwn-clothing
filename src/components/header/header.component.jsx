9cb5dc96acf2f5f594f03dc0744145aed5e6c8d6
JJ Williams <jwilliams722@gmail.com>
2021-09-14 15:38:39 -0600

    Add auth components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,6 +15,10 @@ const Header = () => (
       <Link className='option' to='/shop'>
         CONTACT      
       </Link>
+
+      <Link className='option' to='/signin'>
+        SIGN IN      
+      </Link>
     </div>
   </div>
 );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,8 @@
+import SignIn from '../../components/sign-in/sign-in.component';
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+  <SignIn />
+);
+
+export default SignInAndSignUpPage;
